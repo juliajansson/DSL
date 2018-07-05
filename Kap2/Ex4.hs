63819870d0bb0249985f61a8ae270f1525617547
Julia Jansson <julia.jansson@gmail.com>
2018-07-05 12:07:41 +0200

    New exercise

---
+++ Kap2/Ex4.hs
@@ -0,0 +1,31 @@
+module Ex4 where
+type And p q = (p,q)
+
+andIntro:: p -> q -> And p q
+andIntro p q = (p,q)
+
+andElimL:: And p q -> p
+andElimL s = fst s
+
+andElimR:: And p q -> q
+andElimR s = snd s
+
+type Impl p q = (p -> q)
+
+implIntro:: (p -> q) -> Impl p q
+implIntro f = f
+
+implElim:: Impl p q -> p -> q
+implElim f = f
+
+type Or p q = Either p q
+
+--How to do or stuff?
+
+--orElim:: Or p q -> (p -> r) -> (q -> r) -> r
+
+orIntroL:: p -> Or p q
+orIntroL p = Left p
+
+orIntroR:: q -> Or p q
+orIntroR q = Right q
