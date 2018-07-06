0aad4567df81265162a71fb54e31d0c0565d6a18
Julia Jansson <julia.jansson@gmail.com>
2018-07-06 15:08:28 +0200

    Some Ex9 fixes

---
+++ Kap1/Ex10.hs
@@ -0,0 +1,4 @@
+--s2p:: (Either b c -> a) -> (b -> a, c -> a)
+--s2b eitherbc2a = 
+
+--p2s:: (b -> a, c -> a) -> (Either b c -> a)
--- Kap1/Ex9.hs
+++ Kap1/Ex9.hs
@@ -1,4 +1,6 @@
-f2p:: a -> (b, c) -> (a -> b, a -> c)
+module Ex9 where
+
+f2p:: (a -> (b, c)) -> (a -> b, a -> c)
 f2p a2bc = (a2b, a2c)
    where a2b = fst . a2bc 
          a2c = snd . a2bc
@@ -8,4 +10,24 @@ p2f (a2b, a2c) = \x -> (a2b x, a2c x) --Lambda funktion
 
 --Mer allmänt:
 --p2f f g = \x -> (f x, g x)
+
+f:: Int -> (Int, String)
+f x = (x + 1, show x)
+
+test0:: Int
+test0 = fst (f2p f) 1000
+
+test1:: String
+test1 = snd (f2p f) 1000
+
+p:: (Int -> Int, Int -> String)
+p = (intf, stringf)
+     where intf x = x + 1
+           stringf x = show x
+
+test2:: (Int, String)
+test2 = p2f p 5
+
+--Verkar funka bra!
+
 
