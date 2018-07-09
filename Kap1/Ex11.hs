912311a394327ee9c2eba8cc69fe33dafb9fbb19
Julia Jansson <julia.jansson@gmail.com>
2018-07-09 09:02:59 +0200

    Cardinality

---
+++ Kap1/Ex11.hs
@@ -0,0 +1,11 @@
+--Cardinality of Bool is 2
+--Cardinality of Weekday is 7
+
+--Let cardinality of a be m
+--Let cardinality of b be n
+
+--The cardinality of Either a b is m+n since we have m Left a alternatives and n Right b alternatives
+
+--The cardinality of (a,b) is m*n since we have n values of b for every a, which are m in cardinality
+
+--The cardinality of a -> b is n^m. For every value of a we have n different values that can be assigned to it.
