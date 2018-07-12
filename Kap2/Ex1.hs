0d7121d7ab2088b3e06caa7b0667d5032e1b0c85
Julia Jansson <julia.jansson@gmail.com>
2018-07-12 15:57:38 +0200

    Facit och funderingar

--- Kap2/Ex1.hs
+++ Kap2/Ex1.hs
@@ -66,17 +66,23 @@ F | F | F      | F      | T
 -}
 
 --Lagen om det uteslutna tredje
-{-
+--TODO: gör själv
+
+--Facit - förstår typ nu:
 exc:: Or p (Not p)
 exc = notElim x
      where x:: Not (Not (Or p (Not p)))
            x = notIntro f
-           f:: Not (Or p (Not p)) -> And q (Not q)
-           f y = orElim 
+           f:: Not (Or p (Not p)) -> And p (Not p)
+           f y = andIntro (p y) (np y)
            --q = Or p (Not p)
--}
-
---andIntro gives a circular proof
+           np:: Not (Or p (Not p)) -> Not p
+           np y = notIntro (\p -> andIntro (orIntroL p) y)
+           p:: Not (Or p (Not p)) -> p
+           p y = notElim (nnp y)
+           nnp:: Not (Or p (Not p)) -> Not (Not p)
+           nnp y = notIntro (\np -> andIntro (orIntroR np) y)
+           
 
 --First, theory exploration
 theory1:: (p -> p') -> (q -> q') -> (And p q -> And p' q')
@@ -104,4 +110,11 @@ example1' aqnq = notElim (notIntro (\hyp -> aqnq))
 --TODO: Vad betyder hyp? hyp verkar vara av typ Not q
 
 --TODO: Theory 3 - går inte ihop sig alls
---theory3:: (q -> p) -> (Not p -> Not q)
+
+{-
+theory3:: (q -> p) -> (Not p -> Not q)
+theory3 q p = notIntro . paqnqf
+      where paqnqf:: Not p -> (p -> And q (Not q))
+            paqnqf np = paqnq
+            paqnq:: p -> And q (Not q)
+-}
