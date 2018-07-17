dec03654698164db833555f78050af5031b3e2ce
Julia Jansson <julia.jansson@gmail.com>
2018-07-17 13:23:45 +0200

    Some TODOs

--- Kap2/Ex5.hs
+++ Kap2/Ex5.hs
@@ -1,3 +1,24 @@
 module Ex5 where
 data Empty
 type Not p = p -> Empty
+
+isEmpty:: Empty -> Empty
+isEMpty evE = evE
+
+--TODO: Implement notIntro
+{-
+notIntro:: (p -> (q, q -> Empty)) -> (p -> Empty)
+notIntro f = g
+
+f:: p -> (q, q -> Empty)
+f p = 
+-}
+
+contraHey:: Empty -> p
+contraHey evE = case evE of {}
+
+--TODO: Prove this
+{-
+ex1:: (q, q -> Empty) -> p
+ex1 = 
+-}
