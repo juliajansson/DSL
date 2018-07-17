c0dd00891d6219b2308a54067b05b7c318ea5d4b
Julia Jansson <julia.jansson@gmail.com>
2018-07-17 13:49:19 +0200

    Exam question

---
+++ Kap2/Ex7.hs
@@ -0,0 +1,12 @@
+--FORALL eps > 0 EXISTS delta > 0 . FORALL x (in the domain of f) . abs(x-c) < delta -> abs(f x - f c) < eps. Then f continuous at c.
+
+--type X = Float
+
+--continuous:: Float -> X -> Prop
+--continuous c X = FORALL eps > 0
+
+--Ball function:
+--ball c r = [x | abs (x - c) < r]
+
+
+--TODO: Syntax? Perhaps a paper question.
